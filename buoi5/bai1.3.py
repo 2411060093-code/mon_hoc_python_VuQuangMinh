8462958ff314ba9974d8549eb87bb255cb991b9e
VuQuangMinh <2411060093@hunre.edu.vn>
2026-09-18 10:02:09 +0700

    Hoan thanh buoi5

---
+++ buoi5/bai1.3.py
@@ -0,0 +1,9 @@
+tuoi = 17
+co_giay_phep = False
+if tuoi >= 18:
+    if co_giay_phep:
+        print("Duoc phep lai xe")
+    else:
+        print("Du tuoi nhung chua co giay phep")
+else:
+    print("Chua du tuoi lai xe")
